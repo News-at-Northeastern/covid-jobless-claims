8bc3fb3169ae19f5bde8276a026e50965bdce7c0
tylermachado <3586466+tylermachado@users.noreply.github.com>
2020-03-26 10:21:32 -0400

    add new data

---
+++ app/js/plugins/boilerplate.js
@@ -0,0 +1,30 @@
+var margin = {top:25, right:100, bottom:40, left:75};
+
+var colors = {
+    bold: ["#d51e2d", "#52CFE5", "#385775", "#FFBF3D", "#6f2b6e", "#00CFB5"],
+    pastel: ["#e59097", "#c0adcc", "#b3cadd", "#a3ceaf", "lavender", "aquamarine", "gold"],
+    procon: ["#cc0000", "#808080", "#000"],
+    political: ["#D41B2C", "#006EB5"]
+}
+
+var chartmeta = {
+    title: "Title goes here",
+    subtitle: "Subtitle goes here",
+    source: "U.S. Department of Labor",
+    note: "Numbers are seasonally adjusted."
+}
+
+
+d3.json('/interactive/2020/03/jobless-claims/data/jobless_claims.json')
+  .then(function(data) {
+     data.forEach(function(d) {
+        d["Jobless Claims"] = +d["Jobless Claims"]
+     })
+
+     console.log(data);
+
+    lineTemplate(data, chartmeta, "#line");
+
+}).catch(function(error){
+   // handle error
+});
